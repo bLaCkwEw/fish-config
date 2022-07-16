7aaf479a7ebe8f593183463cf75cbf19118db351
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2022-07-16 16:39:09 +0300

    update theme

--- config.fish
+++ config.fish
@@ -2,5 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Set vim as default text editor
-set -gx EDITOR (type -p vim)
+# Set nvim as default text editor
+set -gx EDITOR (type -p nvim)
+
+# pnpm
+set -gx PNPM_HOME "/home/blackwew/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
